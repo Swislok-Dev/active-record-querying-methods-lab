815b35a95f8901cf9734b5330a5eb24b00630c33
Cory M <cmarker4@gmail.com>
2021-01-17 20:50:16 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,29 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where(rating: self.highest_rating).first
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.where(rating: self.lowest_rating).first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.all.map {|pop| pop if pop.rating > 5}.compact
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
 end
